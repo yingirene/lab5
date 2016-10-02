a5c9598017f1eeda5fc7fc3b7b9647e6505469ef
Philip Guo <philip@pgbovine.net>
2016-10-02 12:07:51 -0700

    minor updates

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,6 @@
 var data = require("../data.json");
 
-exports.addFriend = function(req, res) {    
+exports.addFriend = function(req, res) {
 	// Your code goes here
- }
+  console.log("yay, addFriend just ran!");
+}
